69309427c781f952abf0877ab1232e0f6edafb65
Vasiliy <cenetik@gmail.com>
2025-09-21 20:56:35 +1000

    Добавил проект тестов и начал делать форму со списком текстов

---
+++ FastTextNext/ViewModels/ListTextModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastTextNext.ViewModels
+{
+    public partial class ListTextModel : ObservableObject
+    {
+        [RelayCommand]
+        public void GotoText()
+        {
+          
+        }
+
+        [RelayCommand]
+        public void Find()
+        {
+
+        }
+
+        [ObservableProperty]
+        private string _findText = string.Empty;
+    }
+}
